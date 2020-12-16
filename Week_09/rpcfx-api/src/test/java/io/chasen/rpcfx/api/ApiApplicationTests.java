0921000960f87bb6601a68c9234adc4658d40143
zhangchunsheng20 <zhangchunsheng20@jd.com>
2020-12-16 21:16:35 +0800

    项目初始胡

---
+++ Week_09/rpcfx-api/src/test/java/io/chasen/rpcfx/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chasen.rpcfx.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Week_09/rpcfx-core/src/test/java/io/chasen/rpcfx/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chasen.rpcfx.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
